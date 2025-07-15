fe43e06d548dd29c25bb5124f59593703f0c1927
Nitiroll71 <dirolgofon@gmail.com>
2025-07-15 21:50:36 +0300

    Загрузка на прямую через ссылку

---
+++ my_telebot.py
@@ -0,0 +1,35 @@
+import telebot
+import os
+import time
+import requests
+from dotenv import load_dotenv
+from io import BytesIO
+
+class TelegramBot:    
+
+    def __init__(self, session=None):
+        
+        # Загружаем переменные окружения из .env (токен и id канала)
+        load_dotenv()
+        
+        self.CHANEL_ID=os.getenv('CHANEL_ID')
+        TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+        self.bot = telebot.TeleBot(TELEGRAM_TOKEN)
+        self.session = session or requests.Session()
+
+
+    def send_to_tg_urls(self, urls):
+
+        # загрузка всех фоток из папки
+        for i, url in enumerate(urls):
+            try:
+                response = self.session.get(url)
+                if response.status_code == 200:
+                    photo = BytesIO(response.content)
+                    photo.name = f"picture{i}.png"
+                    self.bot.send_photo(self.CHANNEL_ID, photo)
+                    print(f"Фото {i} отправлено")
+                else:
+                    print(f"Не удалось скачать фото {i}, статус: {response.status_code}")
+            except Exception as e:
+                print(f"Ошибка при отправке фото {i}: {e}")
