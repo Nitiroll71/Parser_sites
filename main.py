d6b3e6019b374e08298b95f15ca2492661e70a11
Nitiroll71 <dirolgofon@gmail.com>
2025-07-16 18:39:38 +0300

    Парсинг с заданного к-ва страниц

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ tgbot = TelegramBot()
 parser_pict.authorization('https://auth.fonwall.ru/auth/login',
                     'https://fonwall.ru/@meow1234/')
 
-picture_urls = parser_pict.get_pictures_url(parser_pict.find_pict_block())
+picture_urls = parser_pict.get_pictures_url(parser_pict.find_pict_block(10))
 
 tgbot = TelegramBot(session=parser_pict.session)
 tgbot.send_to_tg_urls(picture_urls)
--- my_parser.py
+++ my_parser.py
@@ -55,12 +55,21 @@ class Parser:
             chek_user = user_soup.find('span', class_='ProfileHeader_profileFirstName__1G8fe').text
             print(f'Имя пользователя: {chek_user}')
     
-    def find_pict_block(self):
+    def find_pict_block(self, page_count):
         
-        # Ищем все картинки на странице
-        page_block = self.session.get(self.url_site, headers=self.header).text
-        all_pic_block = BeautifulSoup(page_block, 'lxml')
-        all_pic_soup = all_pic_block.find_all('img', class_ = re.compile(r'^Article_image__I_3mF'))
+        MAX_PAGE_COUNT = 5275
+        if page_count < MAX_PAGE_COUNT:
+            all_pic_soup = []
+            for i in range(1, page_count + 1, 1):
+                # Ищем все картинки на странице
+                page_block = self.session.get(f'{self.url_site}?page={i}', headers=self.header).text
+                all_pic_block = BeautifulSoup(page_block, 'lxml')
+                found_imgs_block = all_pic_block.find_all('img', class_ = re.compile(r'^Article_image__I_3mF'))
+                
+                all_pic_soup.extend(found_imgs_block)
+        else:
+            print(f'Введено больше страниц чем есть. Максимум: {MAX_PAGE_COUNT}')
+        
         
         return all_pic_soup
     
@@ -71,6 +80,7 @@ class Parser:
         for i, img in enumerate(block, start=1):
             src = img.get('src')
             urls_pict.append(src)
+            print(f'Картинка {i}: {src}')
         
         # возвращаем ссылки на картинки
         return urls_pict
